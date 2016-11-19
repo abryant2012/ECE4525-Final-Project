4295bb186efb8cfade3cc730e6d3278371cdef23
Andrew Bryant <andrewpb@vt.edu>
2016-11-18 23:58:10 -0500

    Update template.js

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ var sketchProc=function(processingInstance){ with (processingInstance){
 size(400, 400); 
 frameRate(60);
 	
-draw = function(){
+var draw = function(){
 	background(255, 0, 0);	
 };
 }};
